88d83b1d9507a1c9c0a0084b0acd482ea36f5239
Yeahoon-Kim <apple8718@naver.com>
2022-12-01 06:42:37 +0900

    Basic code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <csignal>
 #include <unordered_map>
+#include <iomanip>
+#include <thread>
+#include <atomic>
+#include <chrono>
+#include <mutex>
 #include <tuple>
 #include <pcap.h>
 
@@ -14,48 +19,68 @@ void interruptHandler(const int signo) {
 }
 
 using namespace std;
+std::mutex mutexer;
+std::atomic<bool> isEnd(false); 
 
-enum ENC {
-    OPN, WEP, WPA, WPA2, WPA3
-};
+std::tuple<uint32_t, std::string> parsingTags(const char* payload, const int payloadLength) {
+    int channel, currentIdx = 0, tagLength;
+    char const* reader = payload;
 
-enum CIPHER {
-    CCMP, 
-};
-
-// unordered_map<Mac, tuple<int, int, int, int, int, int, int, int, int, >>
-
-int manageTag(u_char* packet, int idx) {
-    switch(packet[idx]) {
-    case 0:     // SSID parameter set
-    case 1:     // Supported Rates
-    case 3:     // DS Parameter set
-    case 5:     // Traffic Indication Map(TIM)
-    case 42:    // ERP Information
-    case 48:    // RSN Information
-    case 50:    // Extended Supported Rates
-    case 45:    // HT Capabilities
-    case 61:    // HT Information
-    case 127:   // Extended Capabilities
-    case 221:   // Vender Specific
-    default:
-    }
-}
-
-std::string findESSID(char* payload) {
-    if(payload[0] != 0) return NULL;
-    return std::string(payload + 2, payload[1]);
-}
-
-bool parsingTags(const char* payload, const int payloadLength) {
     std::string ESSID;
-    int channel;
 
-    while( true ) {
+    // Find SSID
+    tagLength = reader[1];
+    ESSID = std::string(reader + 2, tagLength);
+    currentIdx += (2 + tagLength);
 
+    while( currentIdx < payloadLength ) {
+        tagLength = reader[1];
+
+        switch(reader[0]) {
+            case 1:     // Supported Rates
+                break;
+            case 3:     // DS Parameter set
+                channel = reader[2];
+                break;
+            case 5:     // Traffic Indication Map(TIM)
+            case 42:    // ERP Information
+            case 48:    // RSN Information
+            case 50:    // Extended Supported Rates
+            case 45:    // HT Capabilities
+            case 61:    // HT Information
+            case 127:   // Extended Capabilities
+            case 221:   // Vender Specific
+
+            default: break;
+        }
+
+        reader += tagLength + 2;
+        currentIdx += tagLength + 2;
     }
 
+    return std::tuple<uint32_t, std::string>(channel, ESSID);
+}
 
+void printScreen(const unordered_map<Mac, std::pair<uint32_t, std::tuple<uint32_t, std::string>>>& m) {
+    using namespace std::chrono_literals;
+    
+    while(not isEnd.load()) {
+        cout << "\033[2J\033[1;1H"; // Clear screen
+        cout << "\nBSSID              PWR   Beacons  ESSID\n";
+
+        [m]() {
+            std::lock_guard<std::mutex> locker(mutexer);
+
+            for(const auto& line: m) {
+                std::cout << (std::string)line.first << "  ";
+                std::cout << std::setw(4) << line.second.first << "  "; 
+                std::cout << std::setw(7) << std::get<0>(line.second.second) << "  ";
+                std::cout << std::get<1>(line.second.second) << '\n';
+            }
+        }();
+
+        std::this_thread::sleep_for(100ms);
+    }
 }
 
 bool airodump(const char* dev, pcap_t* pcap) {
@@ -71,8 +96,12 @@ bool airodump(const char* dev, pcap_t* pcap) {
     Mac BSSID;
     
     char* taggedParameter;
+    std::tuple<uint32_t, std::string> contents;
+    unordered_map<Mac, std::pair<uint32_t, std::tuple<uint32_t, std::string>>> DB;
     
     int res, flag;
+
+    std::thread printer = std::thread(printScreen, DB);
 
     while( true ) {
         res = pcap_next_ex(pcap, &header, &packet);
@@ -93,7 +122,7 @@ bool airodump(const char* dev, pcap_t* pcap) {
         pRadioTapHeader = (PRadiotapHdr)packet;
 
         // Cast beacon frame header
-        radioHdrLength = pRadioTapHeader->it_len;
+        radioHdrLength = ntohs(pRadioTapHeader->it_len);
         pBeaconFrame = (PBeacon)(packet + radioHdrLength);
 
         // Check if the frame is Beacon
@@ -107,11 +136,16 @@ bool airodump(const char* dev, pcap_t* pcap) {
         BSSID = pBeaconFrame->BSSID;
         tagLength = packetLength - radioHdrLength - sizeof(struct beacon_frame) - sizeof(struct fixed_param);
 
-
+        auto it = DB.find(BSSID);
+        if(it != DB.end()) it->second.first++;
+        else {
+            contents = parsingTags(taggedParameter, tagLength);
+            [BSSID, contents](unordered_map<Mac, std::pair<uint32_t, std::tuple<uint32_t, std::string>>> DB) {
+                std::lock_guard<std::mutex> locker(mutexer);
+                DB[BSSID] = {0, contents};
+            }(DB);
+        }
     }
-
-    
-
 }
 
 int main(int argc, char* argv[]) {
@@ -140,7 +174,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // airodump(dev);
+    airodump(dev, pcap);
 
     pcap_close(pcap);
 }
