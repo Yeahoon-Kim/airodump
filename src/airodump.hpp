efccb6e2c3a75094f62527c958a986dd51d109d8
Yeahoon-Kim <apple8718@naver.com>
2022-12-01 06:54:04 +0900

    Rearrange

---
+++ src/airodump.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <csignal>
+#include <unordered_map>
+#include <iomanip>
+#include <thread>
+#include <atomic>
+#include <chrono>
+#include <mutex>
+#include <tuple>
+#include <pcap.h>
+
+#include "radiotap.hpp"
+
+extern std::mutex mutexer;
+extern std::atomic<bool> isEnd(false); 
+
+std::tuple<uint32_t, std::string> parsingTags(const char* payload, const int payloadLength);
+
+void printScreen(const std::unordered_map<Mac, std::pair<uint32_t, std::tuple<uint32_t, std::string>>>& m);
+
+bool airodump(const char* dev, pcap_t* pcap);
